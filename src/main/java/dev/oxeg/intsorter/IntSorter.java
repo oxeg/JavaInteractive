a3b7a24c3a6b5ff2379087e1be5d39f786316604
egor shubin <shubin.egor@gmail.com>
2022-10-16 23:43:16 +0200

    added IntSorter task javadoc description

--- src/main/java/dev/oxeg/intsorter/IntSorter.java
+++ src/main/java/dev/oxeg/intsorter/IntSorter.java
@@ -1,5 +1,12 @@
 package dev.oxeg.intsorter;
 
 public interface IntSorter {
+    /**
+     * Sort an array of integer numbers.
+     * Integer values are within range [1..1000] (both inclusive).
+     * Values can repeate.
+     * @param array array of shuffeled integer values
+     * @return sorted array
+     */
     int[] sort(int[] array);
 }
