a677d135694ae0d1f23f4983254e40e343d295ba
BerserkZak <me4anicman@gmail.com>
2022-10-18 23:03:15 +0300

    Updated IntSorter implementation from BerserkZak

--- src/main/java/dev/oxeg/intsorter/berserkzak/BerserkZakImplementation.java
+++ src/main/java/dev/oxeg/intsorter/berserkzak/BerserkZakImplementation.java
@@ -3,7 +3,6 @@ package dev.oxeg.intsorter.berserkzak;
 import dev.oxeg.intsorter.IntSorter;
 public class BerserkZakImplementation implements IntSorter {
 
-
     @Override
     public int[] sort(int[] array) {
         for (int i=0; i<array.length; i++) {
