b81a8be86c2c869b8878fa8b9677dd8689419977
egor shubin <shubin.egor@gmail.com>
2022-10-21 00:39:47 +0200

    task2 - junior implementation

--- src/main/java/dev/oxeg/fileseparator/oxeg/OxegFileSeparator.java
+++ src/main/java/dev/oxeg/fileseparator/oxeg/OxegFileSeparator.java
@@ -3,10 +3,11 @@ package dev.oxeg.fileseparator.oxeg;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 
 public class OxegFileSeparator {
-    private static final String DIRECTORY = "src/main/java/dev/oxeg/fileseparator/";
+    static final String DIRECTORY = "src/main/java/dev/oxeg/fileseparator/";
     private static final String INPUT_FILENAME = "unsorted.txt";
     private static final String OUTPUT_FILENAME_NUMBERS = "numbers.txt";
     private static final String OUTPUT_FILENAME_LETTERS = "letters.txt";
@@ -15,11 +16,11 @@ public class OxegFileSeparator {
         try (var reader = new FileReader(DIRECTORY + INPUT_FILENAME, StandardCharsets.UTF_8);
              var numbersWriter = new FileWriter(DIRECTORY + OUTPUT_FILENAME_NUMBERS, StandardCharsets.UTF_8);
              var lettersWriter = new FileWriter(DIRECTORY + OUTPUT_FILENAME_LETTERS, StandardCharsets.UTF_8)) {
-            var buffer = new char[1000];
+            var buffer = CharBuffer.allocate(1000);
             var letterCounter = 0;
             var numberCounter = 0;
             while (reader.read(buffer) > 0) {
-                for (char c : buffer) {
+                for (char c : buffer.array()) {
                     if (Character.isDigit(c)) {
                         numberCounter++;
                         numbersWriter.append(c);
@@ -33,6 +34,7 @@ public class OxegFileSeparator {
                 }
                 numbersWriter.flush();
                 lettersWriter.flush();
+                buffer.clear();
             }
             System.out.println("Total Letters: " + letterCounter);
             System.out.println("Total Numbers: " + numberCounter);
