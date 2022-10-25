a86cf493afc219bf52dbff8767c8ac621efe3637
BerserkZak <me4anicman@gmail.com>
2022-10-25 19:43:39 +0300

    FileSeparator from BerserkZak with corrections.

--- src/main/java/dev/oxeg/fileseparator/BerserkZak/BerserkZakFileSeparator.java
+++ src/main/java/dev/oxeg/fileseparator/BerserkZak/BerserkZakFileSeparator.java
@@ -11,6 +11,7 @@ public class BerserkZakFileSeparator {
         FileReader read = new FileReader(fileDefault, StandardCharsets.UTF_8);
         char[] buffer = new char[1000];
         read.read(buffer);
+        read.close();
         StringBuilder numbers = new StringBuilder("Numbers: ");
         StringBuilder letters = new StringBuilder("Letters: ");
         for (int i = 0; i < buffer.length; i++) {
@@ -20,18 +21,34 @@ public class BerserkZakFileSeparator {
                 letters.append(buffer[i]);
             }
         }
+
         File fLetters = new File("C:\\Generate\\Letters.txt");
-        FileWriter wLetters = new FileWriter(fLetters, StandardCharsets.UTF_8);
-        fileDefault.createNewFile();
-        wLetters.write(String.valueOf(letters));
-        wLetters.flush();
-        wLetters.close();
         File fNumbers = new File("C:\\Generate\\Numbers.txt");
-        FileWriter wNumbers = new FileWriter(fNumbers, StandardCharsets.UTF_8);
-        fileDefault.createNewFile();
-        wNumbers.write(String.valueOf(numbers));
-        wNumbers.flush();
-        wNumbers.close();
+
+        FileWriter wLetters = null;
+        try {
+            wLetters = new FileWriter(fLetters, StandardCharsets.UTF_8);
+            fileDefault.createNewFile();
+            wLetters.write(String.valueOf(letters));
+            wLetters.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            wLetters.close();
+        }
+
+        FileWriter wNumbers = null;
+        try {
+            wNumbers = new FileWriter(fNumbers, StandardCharsets.UTF_8);
+            fileDefault.createNewFile();
+            wNumbers.write(String.valueOf(numbers));
+            wNumbers.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            wNumbers.close();
+        }
+
         String numbersCount = String.valueOf(numbers.length());
         String lettersCount = String.valueOf(letters.length());
         System.out.println("Total number of numbers in a string: " + numbersCount);
--- src/main/java/dev/oxeg/fileseparator/BerserkZak/RandomGenerator.java
+++ src/main/java/dev/oxeg/fileseparator/BerserkZak/RandomGenerator.java
@@ -3,26 +3,30 @@ package dev.oxeg.fileseparator.BerserkZak;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 public class RandomGenerator {
-public static void generator() {
+    public static void generator() {
 
+        PrintWriter writer = null;
         try {
-            PrintWriter writer = new PrintWriter("C:\\Generate\\Unsorted.txt", StandardCharsets.UTF_8);
-
+            writer = new PrintWriter("C:\\Generate\\Unsorted.txt", StandardCharsets.UTF_8);
             String generation = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
             StringBuilder sStringGeneration = new StringBuilder();
+            var genRandom = new Random();
+
             for (int n = 0; n <= 999; n++) {
-                int index = (int)(generation.length() * Math.random());
+
+                int index = genRandom.nextInt(generation.length());
                 sStringGeneration.append(generation.charAt(index));
             }
-            writer.printf(String.valueOf(sStringGeneration));
-            writer.close();
 
+            writer.printf(String.valueOf(sStringGeneration));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            writer.close();
         }
-
-}
+    }
 }
